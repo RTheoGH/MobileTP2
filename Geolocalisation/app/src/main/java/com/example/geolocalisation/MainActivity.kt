1878726966e7f5a60b4e84b6c772467f16f1c589
RTheoGH <reynier.theo@gmail.com>
2025-02-18 16:24:38 +0100

    Geolocalisation !

---
+++ Geolocalisation/app/src/main/java/com/example/geolocalisation/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.geolocalisation
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.geolocalisation.ui.theme.GeolocalisationTheme
+import com.google.android.gms.location.LocationServices
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GeolocalisationTheme {
+                Scaffold(topBar = {
+                    TopAppBar(
+                        colors = topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            titleContentColor = MaterialTheme.colorScheme.primary,
+                        ),
+                        title = {
+                            Text("Géolocalisation")
+                        }
+                    )
+                }) { innerPadding ->
+                    Geo(innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Geo(innerPadding: PaddingValues) {
+    val ctx = LocalContext.current
+    val locationClient = remember { LocationServices.getFusedLocationProviderClient(ctx) }
+    var locationText by remember { mutableStateOf("Recherche...") }
+
+    var permissionGranted by remember { mutableStateOf(false) }
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { granted ->
+            permissionGranted = granted
+        }
+    )
+
+    LaunchedEffect(Unit) {
+        when {
+            ContextCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
+                permissionGranted = true
+            }else -> {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+
+        if(permissionGranted){
+            locationClient.lastLocation.addOnSuccessListener{location: Location? ->
+                location?.let {
+                    locationText = "Lat : ${it.latitude}, Long : ${it.longitude}"
+                }?: run {
+                    locationText = "Localisation impossible"
+                }
+            }
+        }else {
+            locationText = "Permission non accordée"
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Text(text=locationText,modifier = Modifier.padding(16.dp))
+    }
+}
+
